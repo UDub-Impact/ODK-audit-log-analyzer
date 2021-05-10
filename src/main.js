64c3fe570f66b118cecae7fbeb8725ed00429205
Naisan Noorassa <naisan99@gmail.com>
2021-05-09 23:10:03 -0700

    Add basic processing of csv file in main.js and use d3 to create bar chart

--- src/main.js
+++ src/main.js
@@ -1,114 +1,194 @@
 // Creates input element that allows user to select audit file and analyzes selected file
 function getAndProcessAuditData() {
-  var input = document.createElement('input');
-  input.type = 'file';
-  
-  var content;
-  input.onchange = e => { 
+  	let input = document.createElement('input');
+  	input.type = 'file';
 
-    // get the file reference
-    var file = e.target.files[0]; 
+	input.onchange = e => { 
 
-    // set up FileReader
-    var reader = new FileReader();
-    reader.readAsText(file,'UTF-8');
+    	// get the file reference
+    	let file = e.target.files[0]; 
 
-    // read file and output content
-    reader.onload = readerEvent => {
-      auditStr = readerEvent.target.result;
-      processAuditFile(auditStr);
+    	// set up FileReader
+    	let reader = new FileReader();
+    	reader.readAsText(file,'UTF-8');
+
+    	// read file and output content
+    	reader.onload = readerEvent => {
+    		auditStr = readerEvent.target.result;
+      		processAuditFile(auditStr);
+    	}
     }
 
-  }
-
-  input.click();
+	input.click();
 }
 
 // Takes String contents of CSV audit file, parses and processes it, and updates page to show insights
 function processAuditFile(auditStr) {
-  var auditArray = CSVToArray(auditStr);
-  document.write(auditArray);
+	// auditData is a list of dictionaries s.t. each dictionary represents a row of the original audit CSV data
+	let auditData = d3.csvParse(auditStr);
+
+	// rows of the original CSV audit have events from many different submissions jumbled together
+	// this function groups all events by submission
+	// this gives us a dictionary that maps submission id to events for that submission
+	let groupedData = groupAuditData(auditData);
+
+	let groupedSubmissionTimes = calculateSubmissionTimes(groupedData);
+	let averageQuestionTimes = calculateAverageQuestionTimes(groupedSubmissionTimes);
+
+	showAverageQuestionTimes(averageQuestionTimes);
 }
 
-// Function borrowed from https://www.bennadel.com/blog/1504-ask-ben-parsing-csv-strings-with-javascript-exec-regular-expression-command.htm
-function CSVToArray(strData, strDelimiter){
-	// Check to see if the delimiter is defined. If not,
-	// then default to comma.
-	strDelimiter = (strDelimiter || ",");
+// Takes a list of dictionaries with each dictionary corresponding to an event of an ODK audit file
+// Returns a dictionary that maps a submission id to events for that submission s.t. events for each submission 
+// are listed in a dictionary mapping question name (node) to a list of events for that question
+// eg.  {
+//			"instanceID1":
+//				{
+//					"question1": [event, event, event],
+//					"question2": [event ,event, event],
+//				},
+//			"instanceID2": {...},
+//      }
+// Ignores any events that aren't associated with an instanceID and node (question name)
+function groupAuditData(auditData) {
+	let groupedData = {};
+	for (const event of auditData) {
+		let instanceID = event["instanceID"];
+		let node = event["node"];
 
-	// Create a regular expression to parse the CSV values.
-	var objPattern = new RegExp(
-		(
-			// Delimiters.
-			"(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+		// we skip events that aren't associated with an instanceID and a question
+		if (instanceID && node) {
+			if (!(instanceID in groupedData)) {
+				groupedData[instanceID] = {};
+			}
 
-			// Quoted fields.
-			"(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+			if (!(node in groupedData[instanceID])) {
+				groupedData[instanceID][node] = [];
+			}
 
-			// Standard fields.
-			"([^\"\\" + strDelimiter + "\\r\\n]*))"
-		),
-		"gi"
-		);
-
-
-	// Create an array to hold our data. Give the array
-	// a default empty first row.
-	var arrData = [[]];
-
-	// Create an array to hold our individual pattern
-	// matching groups.
-	var arrMatches = null;
-
-
-	// Keep looping over the regular expression matches
-	// until we can no longer find a match.
-	while (arrMatches = objPattern.exec( strData )){
-
-		// Get the delimiter that was found.
-		var strMatchedDelimiter = arrMatches[ 1 ];
-
-		// Check to see if the given delimiter has a length
-		// (is not the start of string) and if it matches
-		// field delimiter. If id does not, then we know
-		// that this delimiter is a row delimiter.
-		if (
-			strMatchedDelimiter.length &&
-			(strMatchedDelimiter != strDelimiter)
-			){
-
-			// Since we have reached a new row of data,
-			// add an empty row to our data array.
-			arrData.push( [] );
-
+			groupedData[instanceID][node].push(event);
 		}
-
-
-		// Now that we have our delimiter out of the way,
-		// let's check to see which kind of value we
-		// captured (quoted or unquoted).
-		if (arrMatches[ 2 ]){
-
-			// We found a quoted value. When we capture
-			// this value, unescape any double quotes.
-			var strMatchedValue = arrMatches[ 2 ].replace(
-				new RegExp( "\"\"", "g" ),
-				"\""
-				);
-
-		} else {
-
-			// We found a non-quoted value.
-			var strMatchedValue = arrMatches[ 3 ];
-
-		}
-
-
-		// Now that we have our value string, let's add
-		// it to the data array.
-		arrData[ arrData.length - 1 ].push( strMatchedValue );
 	}
 
-	// Return the parsed data.
-	return( arrData );
+	return groupedData;
+}
+
+// Takes grouped audit dictionary returned by groupAuditData
+// Returns a grouped dictionary of the same format s.t. each question is mapped to its total time
+// eg.  {
+//			"instanceID1":
+//				{
+//					"question1": time1,
+//					"question2": time2,
+//				},
+//			"instanceID2": {...},
+//      }
+function calculateSubmissionTimes(groupedData) {
+	let submissionTimes = {};
+	for (const [instanceID, questions] of Object.entries(groupedData)) {
+		submissionTimes[instanceID] = {};
+
+		for (const [node, events] of Object.entries(questions)) {
+			submissionTimes[instanceID][node] = calculateQuestionTime(events);
+		}
+	}
+
+	return submissionTimes;
+}
+
+// Takes grouped submission times returned by calculateSubmissionTimes
+// Returnes a dictionary mapping question name to the average response time for that question
+function calculateAverageQuestionTimes(groupedSubmissionTimes) {
+	let questionTime = {};
+	let questionResponses = {};
+
+	for (const [instanceID, questions] of Object.entries(groupedSubmissionTimes)) {
+		for (const [node, time] of Object.entries(questions)) {
+			if (!(node in questionTime)) {
+				questionTime[node] = 0;
+				questionResponses[node] = 0;
+			}
+
+			questionTime[node] += time;
+			questionResponses[node]++;
+		}
+	}
+
+	let questionAverages = {};
+	for (const node of Object.keys(questionTime)) {
+		questionAverages[node] = questionTime[node] / questionResponses[node];
+	}
+
+	return questionAverages;
+}
+
+// Takes averageQuestionTimes as returned by calculateAverageQuestionTimes
+// Displays average question times in a bar chart
+function showAverageQuestionTimes(averageQuestionTimes) {
+	// Converts dictionary to a list of dictionaries with node and average attrs
+	var mapped = Object.keys(averageQuestionTimes).map(function(node) {
+		return {
+			node: node,
+			average: averageQuestionTimes[node],
+		};
+	});
+	
+
+	var margin = {top: 50, right: 0, bottom: 10, left: 50};
+	var width = 600 - margin.left - margin.right;
+    var height = 400 - margin.top - margin.bottom;
+
+	var x = d3.scaleBand()
+		.range([0, width])
+		.round(0.5);
+	var y = d3.scaleLinear()
+		.range([height, 0]);
+
+	var xAxis = d3.axisTop(x);
+	var yAxis = d3.axisLeft(y);
+	
+	x.domain(mapped.map(d => d.node));
+	y.domain([0, d3.max(mapped, d => d.average)]);
+	
+	// Hard-coded "average-question-time" id here could be changed
+	var svg = d3.select("#average-question-time").append("svg")
+	    .attr("width", 600)
+	    .attr("height", 400)
+		.append("g")
+		.attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+	
+
+	// Insert graph
+	svg.selectAll("bar")
+	    .data(mapped)
+	    .enter().append("rect")
+	      .style("fill", "steelblue")
+	      .attr("x", d => x(d.node))
+	      .attr("width", width / mapped.length)
+	      .attr("y", d => y(d.average))
+	      .attr("height", d => height - y(d.average));
+
+	// Insert title text
+	svg.append("text")
+	  	.attr("x", (width / 2))             
+	  	.attr("y", 0 - (margin.top / 2))
+	  	.attr("text-anchor", "middle")  
+	  	.style("font-size", "16px") 
+	  	.text("Average Time Spent on Each Question");
+
+	svg.append("g").call(xAxis);
+	svg.append("g").call(yAxis);
+}
+
+// Takes a list of events corresponding to a single question in one submission
+// Returns the total response time for the question
+function calculateQuestionTime(events) {
+	let totalTime = 0;
+	for (const event of events) {
+		if (event["end"] && event["start"]) {
+			totalTime += event["end"] - event["start"];
+		}
+	}
+
+	return totalTime;
 }
